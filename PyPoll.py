9c239d9179001132e7314f6a29fd1581a5f394d8
Ken P <87345178+airplane3333@users.noreply.github.com>
2021-09-28 19:28:06 -0700

    Update file with text message

--- PyPoll.py
+++ PyPoll.py
@@ -39,53 +39,61 @@ with open(file_to_load) as election_data:
             candidate_votes[canidate_name] = 0
         #add votes to that candidate vote count
         candidate_votes[canidate_name] +=1
+#save the results to a text file to print off
+with open(file_to_save, "w") as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"--------------------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"--------------------------------------\n")
+    print(election_results, end="")
+    #save election_results to text file
+    txt_file.write(election_results)
 
-#get % of votes by each candidate by looping thru the counts
-#iterate throught the candidate_votes
-for candidate_name in candidate_votes:
-    votes = candidate_votes[candidate_name]
-    vote_percentage = float(votes)/float(total_votes) * 100
-    #print(f"{candidate_name}: received {vote_percentage:.2f}% for the vote")
-    print(f"{candidate_name}: {vote_percentage:.2f} ({votes:,})\n")
+    #get % of votes by each candidate by looping thru the counts
+    #iterate throught the candidate_votes
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = float(votes)/float(total_votes) * 100
+        #print(f"{candidate_name}: received {vote_percentage:.2f}% for the vote")
+        candidate_results = (f"{candidate_name}: {vote_percentage:.2f} ({votes:,})\n")
+        #save results to text file and print to terminal 
+        txt_file.write(candidate_results)
+        print(candidate_results)
 
-    #get winning vote count and candidate
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        #if true then setn count and % to winning variables
-        winning_count = votes
-        winning_percentage = vote_percentage
-        #set the winning candidate to the candidates name
-        winning_candidate = candidate_name
-#output to terminal for winning summary
-winning_candidate_summary = (
-    f"--------------------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.2f}%\n"
-    f"--------------------------------------\n")
-print(winning_candidate_summary)
-#print the total votes
-#print(candidate_votes)
-#print(total_votes)
-#print(candidate_options)
+        #get winning vote count and candidate
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            #if true then setn count and % to winning variables
+            winning_count = votes
+            winning_percentage = vote_percentage
+            #set the winning candidate to the candidates name
+            winning_candidate = candidate_name
+    #output to terminal for winning summary
+    winning_candidate_summary = (
+        f"--------------------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.2f}%\n"
+        f"--------------------------------------\n")
+    txt_file.write(winning_candidate_summary)
+    print(winning_candidate_summary)
+    #print the total votes
+    #print(candidate_votes)
+    #print(total_votes)
+    #print(candidate_options)
 
 
 #close the file
 election_data.close()
 
 #using the open() funtion with the w mode will write data to the file
-with open(file_to_save,"w") as txt_file:
+#with open(file_to_save,"w") as txt_file:
 #output of election resutls
-    txt_file.write("Counties in the election \n-------------------------------------------\n")
-    txt_file.write("Arapahoe \nDenver \nJefferson")
-    txt_file.write("The canidates in the election are: " + str(candidate_options) )
-    txt_file.write("\nThe total votes cast: " +str(total_votes))
+    #txt_file.write("Counties in the election results are: Arapahoe, Denver,Jefferson")
+    #txt_file.write("\nThe canidates in the election are: " + str(candidate_options) )
+    #txt_file.write("\nThe total votes cast: " +str(total_votes)+"\n")
+    #txt_file.write(winning_candidate_summary)
 
 
 #close the file
 txt_file.close()
-
-#Total number of votes cast
-#A complete list of candidates who received votes
-#Total number of votes each candidate received
-#Percentage of votes each candidate won
-#The winner of the election based on popular vote
